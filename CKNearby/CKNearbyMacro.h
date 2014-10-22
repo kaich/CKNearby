a754b34057028a9276c842937806b93f727dfd50
mac <mac@macdeMac-mini.local>
2014-10-22 09:04:30 +0800

    dependency for ckdownloadmanager

--- CKNearby/CKNearbyMacro.h
+++ CKNearby/CKNearbyMacro.h
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014年 weiaipu. All rights reserved.
 //
 
+#define CKNEARBY @"CKNearby_component"
 
 #define SELF_DEVICE_NAME_KEY  @"own_devcie_name"
 #define SELF_DEVICE_NAME_VALUE  [[NSUserDefaults standardUserDefaults] objectForKey:SELF_DEVICE_NAME_KEY]
